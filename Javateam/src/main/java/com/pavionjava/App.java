7fa580fbabd52aeca4d1024daba5548cc55f5fa4
pavithra <pavithragowda2807@gmail.com>
2025-06-06 20:07:10 +0530

    azure pipeline example

---
+++ Javateam/src/main/java/com/pavionjava/App.java
@@ -0,0 +1,13 @@
+package com.pavionjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
